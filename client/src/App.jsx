4f3b58f9c90c8df9d84f57f4b2348d0b2c8cabb4
abhipawar01 <abhishekpawar969696@gmail.com>
2025-09-18 18:39:33 +0530

    Created "Newchat" button "search Conversation" field and displayed recent chats on SideBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import Sidebar from './components/Sidebar'
 import ChatBox from './components/ChatBox'
 import Credits from './pages/Credits'
 import Community from './pages/Community' 
+import { AppContextProvider } from './context/AppContext'
 
 const App = () => {
   return (
-    <>
+    <AppContextProvider>
     <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000]
     dark:text-white'>
+    {/*<div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1" style={{backgroundColor: 'red'}}>*/}
     <div className='flex h-screen w-screen'>
       <Sidebar />
       <Routes>
@@ -17,10 +19,10 @@ const App = () => {
           <Route path= '/Credits' element={<Credits />} />
           <Route path= '/Community' element={<Community />} />
       </Routes>
-    </div>
-    </div>
 
-    </>
+    </div>
+    </div>
+    </AppContextProvider>
   )
 }
 
