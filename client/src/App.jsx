eb0d1cfc7de4d36ddf50e79407a3688a5e88d0d7
abhipawar01 <abhishekpawar969696@gmail.com>
2025-09-20 23:25:38 +0530

    User profile but created with logout option & added the functionality of removing sidebar for mobile devices.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,23 @@ import ChatBox from './components/ChatBox'
 import Credits from './pages/Credits'
 import Community from './pages/Community' 
 import { AppContextProvider } from './context/AppContext'
+import { useState } from 'react'
+import {assets} from './assets/assets'
 
 const App = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
   return (
     <AppContextProvider>
+      {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 
+      w-8 h-8 cursor-pointer md:hiddden not-dark:invert'  onClick={()=> setIsMenuOpen(true)}/>}
     <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000]
     dark:text-white'>
     {/*<div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1" style={{backgroundColor: 'red'}}>*/}
     <div className='flex h-screen w-screen'>
-      <Sidebar />
+      <Sidebar isMenuOpen = {isMenuOpen} setIsMenuOpen = {setIsMenuOpen} />
       <Routes>
           <Route path= '/' element={<ChatBox />} />
           <Route path= '/Credits' element={<Credits />} />
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,13 +3,16 @@ import { useAppContext } from '../context/AppContext';
 import { useState } from 'react';
 import moment from 'moment';
 import { set } from './../../node_modules/moment/src/lib/moment/get-set';
-const Sidebar = () => {
-    const {chats,setSelectedChat, theme, setTheme, user, navigate} = useAppContext();
+
+
+const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
+    const {chats, setSelectedChat, theme, setTheme, user, navigate} = useAppContext();
     const [search, setSearch] = useState('');
   return (
-    <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 
+    <div className= {`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 
     to-[#000000]/30
-    border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1'>
+    border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1
+    ${!isMenuOpen && 'max-md:translate-x-full'}`}>
 
         {/*Logo*/}
         <img src={theme ==='dark' ? assets.logo_full : assets.logo_full_dark} alt=""
@@ -36,7 +39,8 @@ const Sidebar = () => {
             chats.filter((chat)=> chat.messages[0] ? chat.messages[0].content.
             toLowerCase().includes(search.toLowerCase()) : chat.name.toLowerCase().
             includes(search.toLowerCase())).map((chat)=> (
-              <div key={chat._id} className='p-2 px-4 dark:bg-[#573176C]/10 border
+              <div onClick={() => {navigate('/'); setSelectedChat(chat); setIsMenuOpen(false)}}
+              key={chat._id} className='p-2 px-4 dark:bg-[#573176C]/10 border
               border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer
               flex justify-between group'>
                 <div> 
@@ -55,7 +59,7 @@ const Sidebar = () => {
           }
         </div>
         {/*Community Images*/}
-        <div onClick={()=> {navigate('/community')}} className='flex items-center gap-2 p-3 mt-auto border border-gray-300
+        <div onClick={()=> {navigate('/community'); setIsMenuOpen(false)}} className='flex items-center gap-2 p-3 mt-auto border border-gray-300
         dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
           <img src={assets.gallery_icon} className='w-4.5 not-dark:invert' alt=""/>
           <div className='flex flex-col text-sm'>
@@ -63,7 +67,7 @@ const Sidebar = () => {
           </div>
         </div>
         {/*Credits Purchases Option*/}
-        <div onClick={()=> {navigate('/credits')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300
+        <div onClick={()=> {navigate('/credits'); setIsMenuOpen(false)}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300
         dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all'>
           <img src={assets.diamond_icon} className='w-4.5 dark:invert' alt=""/>
           <div className='flex flex-col text-sm'>
@@ -85,10 +89,23 @@ const Sidebar = () => {
           </div>
           <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full
           transition-transform peer-checked:translate-x-4'>
-
           </span>
          </label>
          </div>
+
+         {/*User Profile*/}
+         <div className='flex items-center gap-3 p-3 mt-auto border border-gray-300
+        dark:border-white/15 rounded-md cursor-pointer group'>
+          <img src={assets.user_icon} className='w-7 rounded-full' alt=""/>
+          <p className='flex-1 text-sm dark:text-primary truncate'>{user ? user.name :
+            'Login your account'}</p>
+            {user && <img src={assets.logout_icon} className='h-4 cursor-pointer hidden not-dark:invert 
+            group-hover:block' alt=""/>} 
+        </div>
+
+        <img onClick={() => setIsMenuOpen(false)} src={assets.close_icon} className='absolute top-3 right-3 w-5 h-5 
+        cursor-pointer md:hidden not-dark:invert' alt=""/>
+
  </div>
 )
 
