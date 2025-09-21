98bf439e5e1359347eb215170751e0a1ecf3c0ae
abhipawar01 <abhishekpawar969696@gmail.com>
2025-09-21 21:21:11 +0530

    Created Login And Register page for users. Frontend Completed.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import React, { use } from 'react'
+
 import { Route, Routes , useLocation} from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import ChatBox from './components/ChatBox'
 import Credits from './pages/Credits'
 import Community from './pages/Community' 
-import { AppContextProvider } from './context/AppContext'
+import { AppContextProvider, useAppContext } from './context/AppContext'
 import { useState } from 'react'
 import {assets} from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
-
+import Login from './pages/Login'
 const App = () => {
+
+  const {user} = useAppContext();
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -22,8 +24,11 @@ const App = () => {
     <AppContextProvider>
       {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 
       w-8 h-8 cursor-pointer md:hiddden not-dark:invert'  onClick={()=> setIsMenuOpen(true)}/>}
-    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000]
+
+    {user ? (
+          <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000]
     dark:text-white'>
+
     {/*<div className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1" style={{backgroundColor: 'red'}}>*/}
     <div className='flex h-screen w-screen'>
       <Sidebar isMenuOpen = {isMenuOpen} setIsMenuOpen = {setIsMenuOpen} />
@@ -35,6 +40,13 @@ const App = () => {
 
     </div>
     </div>
+    ) : (
+      <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex
+      items-center justify-center h-screen w-screen'>
+        <Login/>
+      </div>
+    )}
+
     </AppContextProvider>
   )
 }
