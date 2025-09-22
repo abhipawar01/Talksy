7e9b2ec2ef7401dcee790da83be2c743da9cff1f
abhipawar01 <abhishekpawar969696@gmail.com>
2025-09-22 14:28:34 +0530

    Created two API's. one for generating text content and another is for generating image using google gemini API and openai library. And for image generation imagekit is used.

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,7 @@
- 
+import Chat from '../models/Chat.js';
+import User from '../models/User.js'
+import openai from '../configs/openai.js'; 
+import { use } from 'react';
 
 
  //Text-based AI chat Message Controller
@@ -12,6 +15,47 @@
             isImage:false 
         })
 
+        const {choices} = await openai.chat.completions.create({
+        model: "gemini-2.0-flash",
+        messages: [        
+            {
+            role: "user",
+            content: prompt,
+        },
+    ],
+});
+    const reply = {...choices[0].message, timestamp: Date.now(), isImage:false}
+        res.json({success:true, reply})
+        chat.messages.push(reply)
+        await chat.save();
+
+        await User.updateOne({_id:userId}, {$inc:{credits:-1}})
+
+    } catch (error) {
+        res.json({success:false, message:error.message}); 
+    }
+ }
+
+
+ //Image generation Message Controller
+ export const imageMessageController = async (req,res) => {
+    try {
+        const userId = req.user._id
+        //check credits
+        if(req.user.credits < 2){
+            return res.json({success:false, message:"You don't have enough credits to use this feature"});
+        }
+        const {prompt, chatId, isPublished} = req.body
+        //find chat
+        const chat = await Chat.findOne({userId, _id:chatId})
+
+        //Push user message to chat
+        chat.messages.push({
+            role:"user", 
+            content:prompt, 
+            timestamp: Date.now(),
+            isImage:false
+        })
     } catch (error) {
         
     }
