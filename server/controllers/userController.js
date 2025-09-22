5436baaf8039a4c1059304762de8d3a0fcf9d4cc
abhipawar01 <abhishekpawar969696@gmail.com>
2025-09-22 10:52:12 +0530

    Connected backend with mongodb and created user API's and middleware,routes for user.

---
+++ server/controllers/userController.js
@@ -0,0 +1,60 @@
+import e from 'express';    
+import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+//Generate JWT Token
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+
+//API to register a user
+
+export const registreUser = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    try {
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.json({success: false, message:"User already Exists"});
+        }
+        const user = await User.create({name, email, password});
+
+        const token = generateToken(user._id);
+        res.json({success:true , token});
+    } catch (error) {
+        return res.json({success: false, message: error.message}); 
+    }
+}
+
+//API to login a user
+export const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const userExists = await User.findOne({email});
+        if(user){
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if(isMatch){
+                const token = generateToken(user._id);
+                return res.json({success:true , token});
+            }
+        }
+        return res.json({success: false, message: "Invalid Credentials"});
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
+
+//API to get user data
+
+export const getUser = async (req, res) => {
+    try {
+        const user = req.user;
+        return res.json({success:true, user});
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 
+await connectDB();
 //middleware
 app.use(cors());
 app.use(express.json());
 
 //Routes
 app.get('/', (req,res)=> res.send('Server is running...'));
+app.use('/api/user',userRouter);
 
 const PORT = process.env.PORT || 3000;
 
